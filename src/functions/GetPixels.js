2c75be6e596276fa1846741ad6c4f974085634bf
chinmaymokashicm <chinmaymokashicm@gmail.com>
2022-03-11 22:51:08 -0600

    Changed GetPixel function call

--- src/functions/GetPixels.js
+++ src/functions/GetPixels.js
@@ -1,42 +1,4 @@
-// var nj = require("numjs")
-
 import { useState } from "react";
-
-// async function GetPixels(imageURLs, setImgDataArray, arrayImageDimensions) {
-
-//   try {
-//     console.log("Calling getPixels");
-//     console.log("arrayImageDimensions", arrayImageDimensions)
-//     const imgDataArray = [];
-
-//     async function getInfo(objectURL, imageDimensions) {
-//       var img = new Image();
-//       console.log("GetPixels")
-//       var canvas = document.createElement("canvas");
-//       var ctx = canvas.getContext("2d", { colorSpace: "display-p3" });
-//       img.src = objectURL;
-//       // canvas.width = img.width
-//       // canvas.height = img.height
-//       canvas.width = imageDimensions.width
-//       canvas.height = imageDimensions.height
-//       img.onload = async function () {
-//         ctx.drawImage(img, 0, 0);
-//         var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height); //Switched to canvas.width
-//         imgDataArray.push({ array: imgData, width: canvas.width, height: canvas.height });
-//       };
-//       return imgDataArray;
-//     }
-
-//     for (var i = 0; i < imageURLs.length; i++) {
-//       var objectURL = imageURLs[i].objectURL;
-//       var out = await getInfo(objectURL, arrayImageDimensions[i]);
-//     }
-//     setImgDataArray(imgDataArray);
-//   } catch (e) {
-//     console.log(imageURLs)
-//     console.log(e);
-//   }
-// }
 
 function GetPixels(overlayURLs, setImgDataArray) {
   function getPixels(counterImage, counterOverlayName, tempPixelArray) {
