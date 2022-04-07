e577dd38f669bda86fb50b4ff599698eebe02b82
chinmaymokashicm <chinmaymokashicm@gmail.com>
2022-04-07 13:26:43 -0500

    Did not call CreatePixelZip.js file

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -498,7 +498,43 @@ const Carousel = ({
                 <Button
                   text="Pixel Data"
                   onClick={() => {
-                    CreatePixelZip(overlayPixelsArray, stackImageURLs, stackCounter)
+                    const JSZip = require("jszip");
+                    const zip = new JSZip();
+                    const folder = zip.folder("pixels");
+                    for (
+                      let imageCounter = 0;
+                      imageCounter < overlayPixelsArray.length;
+                      imageCounter++
+                    ) {
+                      for (
+                        let overlayCounter = 0;
+                        overlayCounter <
+                        Object.keys(overlayPixelsArray[imageCounter]).length;
+                        overlayCounter++
+                      ) {
+                        var overlayNamesArray = Object.keys(
+                          overlayPixelsArray[imageCounter]
+                        );
+                        var href = CreateCSV(
+                          CreateRGBArray(
+                            overlayPixelsArray[imageCounter][
+                              overlayNamesArray[overlayCounter]
+                            ]
+                          )
+                        );
+                        var filename =
+                          stackImageURLs[stackCounter][imageCounter].imageName +
+                          "_" +
+                          overlayNamesArray[overlayCounter] +
+                          ".csv";
+                        folder.file(filename, href);
+                      }
+                    }
+                    zip
+                      .generateAsync({ type: "blob" })
+                      .then(function (content) {
+                        saveAs(content, "pixels.zip");
+                      });
                   }}
                   disabled={overlayPixelsArray[0] !== undefined ? false : true}
                 />
